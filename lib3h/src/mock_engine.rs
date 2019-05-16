157ace5070e8560cdf716e1de33676412868885f
damien <doute.d@gmail.com>
2019-05-16 12:03:13 -0400

    Remove JsonProtocol refs

--- lib3h/src/mock_engine.rs
+++ lib3h/src/mock_engine.rs
@@ -1,16 +1,26 @@
 
+use std::{
+    collections::{hash_map::Entry, HashMap},
+    convert::TryFrom,
+    sync::{mpsc, Mutex},
+};
+
+use holochain_lib3h_protocol::protocol::Lib3hProtocol;
+use crate::network_engine::NetworkEngine;
+use p2p_protocol::P2pProtocol;
+
 /// Lib3h's 'mock mode' as a NetworkEngine
 struct MockEngine {
     /// End-user's network config
     config: serde_json::Value,
     /// Sender to the Worker
-    tx: mpsc::Sender<Protocol>,
+    tx: mpsc::Sender<Lib3hProtocol>,
     /// identifier
     name: String,
 }
 
 impl MockEngine {
-    pub fn new(config: serde_json::Value, tx: mpsc::Sender<Protocol>) -> NetResult<Self> {
+    pub fn new(config: serde_json::Value, tx: mpsc::Sender<Lib3hProtocol>) -> NetResult<Self> {
         Ok(Lib3hMain {
             config,
             tx,
@@ -19,19 +29,18 @@ impl MockEngine {
     }
     /// Received message from p2p network.
     /// -> Process it or forward to local client
-    fn receive(&mut self, data: Protocol) -> NetResult<()> {
-        self.log
-            .d(&format!("<<<< '{}' p2p recv: {:?}", self.name.clone(), data));
+    fn receive(&mut self, data: P2pProtocol) -> NetResult<()> {
+        println!("(log.d) <<<< '{}' p2p recv: {:?}", self.name.clone(), data);
         // serve only JsonProtocol
-        let maybe_json_msg = JsonProtocol::try_from(&data);
+        let maybe_json_msg = Lib3hProtocol::try_from(&data);
         if maybe_json_msg.is_err() {
             return Ok(());
         };
         // Note: use same order as the enum
         match maybe_json_msg.as_ref().unwrap() {
-            JsonProtocol::SendMessageResult(msg) => {
+            Lib3hProtocol::SendMessageResult(msg) => {
                 // FIXME
-                self.tx.send(data)?;
+                self.tx.send(msg)?;
             }
             _ => {
                 panic ! ("unexpected {:?}", & maybe_json_msg);
@@ -55,13 +64,12 @@ impl NetworkEngine for MockEngine {
         Ok(())
     }
     fn advertise(&self) -> String {
-        "FIXME"
+        "FIXME".to_string()
     }
 
     /// process a message sent by our local Client
-    fn serve(&mut self, data: Protocol) -> NetResult<()> {
-        self.log
-            .d(&format!(">>>> '{}' recv: {:?}", self.name.clone(), data));
+    fn serve(&mut self, data: Lib3hProtocol) -> NetResult<()> {
+        println!("(log.d) >>>> '{}' recv: {:?}", self.name.clone(), data);
         // serve only JsonProtocol
         let maybe_json_msg = JsonProtocol::try_from(&data);
         if maybe_json_msg.is_err() {
@@ -69,56 +77,56 @@ impl NetworkEngine for MockEngine {
         };
         // Note: use same order as the enum
         match maybe_json_msg.as_ref().unwrap() {
-            JsonProtocol::SuccessResult(msg) => {
+            Lib3hProtocol::SuccessResult(msg) => {
                 // FIXME
             }
-            JsonProtocol::FailureResult(msg) => {
+            Lib3hProtocol::FailureResult(msg) => {
                 // FIXME
             }
-            JsonProtocol::TrackDna(msg) => {
+            Lib3hProtocol::TrackDna(msg) => {
                 // FIXME
             }
-            JsonProtocol::UntrackDna(msg) => {
+            Lib3hProtocol::UntrackDna(msg) => {
                 // FIXME
             }
-            JsonProtocol::SendMessage(msg) => {
+            Lib3hProtocol::SendMessage(msg) => {
                 // FIXME
             }
-            JsonProtocol::HandleSendMessageResult(msg) => {
+            Lib3hProtocol::HandleSendMessageResult(msg) => {
                 // FIXME
             }
-            JsonProtocol::FetchEntry(msg) => {
+            Lib3hProtocol::FetchEntry(msg) => {
                 // FIXME
             }
-            JsonProtocol::HandleFetchEntryResult(msg) => {
+            Lib3hProtocol::HandleFetchEntryResult(msg) => {
                 // FIXME
             }
-            JsonProtocol::PublishEntry(msg) => {
+            Lib3hProtocol::PublishEntry(msg) => {
                 // FIXME
             }
-            JsonProtocol::FetchMeta(msg) => {
+            Lib3hProtocol::FetchMeta(msg) => {
                 // FIXME
             }
-            JsonProtocol::HandleFetchMetaResult(msg) => {
+            Lib3hProtocol::HandleFetchMetaResult(msg) => {
                 // FIXME
             }
-            JsonProtocol::PublishMeta(msg) => {
+            Lib3hProtocol::PublishMeta(msg) => {
                 // FIXME
             }
             // Our request for the publish_list has returned
-            JsonProtocol::HandleGetPublishingEntryListResult(msg) => {
+            Lib3hProtocol::HandleGetPublishingEntryListResult(msg) => {
                 // FIXME
             }
             // Our request for the hold_list has returned
-            JsonProtocol::HandleGetHoldingEntryListResult(msg) => {
+            Lib3hProtocol::HandleGetHoldingEntryListResult(msg) => {
                 // FIXME
             }
             // Our request for the publish_meta_list has returned
-            JsonProtocol::HandleGetPublishingMetaListResult(msg) => {
+            Lib3hProtocol::HandleGetPublishingMetaListResult(msg) => {
                 // FIXME
             }
             // Our request for the hold_meta_list has returned
-            JsonProtocol::HandleGetHoldingMetaListResult(msg) => {
+            Lib3hProtocol::HandleGetHoldingMetaListResult(msg) => {
                 // FIXME
             }
             _ => {
--- lib3h/src/network_engine.rs
+++ lib3h/src/network_engine.rs
@@ -1,4 +1,6 @@
 
+use holochain_lib3h_protocol::protocol::Lib3hProtocol;
+
 /// Common interface for all types of network modules to be used by the Lib3hWorker
 pub trait NetworkEngine {
     /// Start network communications
@@ -8,7 +10,7 @@ pub trait NetworkEngine {
     /// Terminate module. Perform some cleanup.
     fn terminate(&self) -> NetResult<()>;
     /// Handle some data message sent by the local Client
-    fn serve(&mut self, data: Protocol) -> NetResult<()>;
+    fn serve(&mut self, data: Lib3hProtocol) -> NetResult<()>;
     /// Get qualified transport address
     fn advertise(&self) -> String;
 }
