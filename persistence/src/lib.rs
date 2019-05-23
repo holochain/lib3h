adf81ae2a973cb45f79630ab385568dae850c47f
Carmelo Piccione <carmelo.piccione@struktured.io>
2019-05-23 02:46:46 -0400

    Add persistence_derive as external crate

--- persistence/src/lib.rs
+++ persistence/src/lib.rs
@@ -24,6 +24,8 @@ extern crate serde;
 extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
+#[macro_use]
+extern crate holochain_persistence_derive;
 extern crate snowflake;
 #[macro_use]
 extern crate regex;
