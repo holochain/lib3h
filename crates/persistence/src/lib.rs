758ae12458cb0901e4db05bbbf787e61e201daf9
Carmelo Piccione <carmelo.piccione@struktured.io>
2019-06-01 20:58:32 -0400

    Document persistence api

--- crates/persistence/src/lib.rs
+++ crates/persistence/src/lib.rs
@@ -1,12 +1,6 @@
-//! Holochain Core Types
-//!
-//! The reason for having this crate is to have a minimal but
-//! complete set of types that are used in most other Holochain
-//! crates, but that don't include Holochain itself.
-//!
-//! Note: This is already quite big. Maybe break the CAS and EAV traits
-//! out into their separate crate as well since those are generic and not
-//! necessarily bound to Holochain.
+//! Holochain Persistence Api
+//! This crate defines apis for a content addressable storage (cas) and a generic 
+//! entity attribute value index (eavi).
 #![feature(try_from)]
 #![feature(try_trait)]
 #![feature(never_type)]
@@ -24,13 +18,8 @@ extern crate serde_json;
 extern crate serde_derive;
 #[macro_use]
 extern crate lib3h_persistence_derive;
-//extern crate snowflake;
-//#[macro_use]
-//#[cfg(test)]
 
-//extern crate hcid;
 extern crate uuid;
-//extern crate wasmi;
 
 pub mod cas;
 pub mod eav;
