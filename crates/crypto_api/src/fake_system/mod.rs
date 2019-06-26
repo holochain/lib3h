e2485d813ceba2673c16fd6d82328f45e15f2914
neonphog <neonphog@gmail.com>
2019-06-26 17:07:25 -0600

    random fix

--- crates/crypto_api/src/fake_system/mod.rs
+++ crates/crypto_api/src/fake_system/mod.rs
@@ -7,7 +7,10 @@ use crate::{Buffer, CryptoRandom, CryptoResult, CryptoSignature, CryptoSystem};
 /// Even the random functions are fake, and produce poor results.
 pub struct FakeCryptoSystem {}
 
-const FAKE_SEQ: [u32; 4] = [0x30698bae, 0x47984c92, 0x901d24fb, 0x91fba506];
+const FAKE_SEQ: [usize; 4] = [0x30698bae, 0x47984c92, 0x901d24fb, 0x91fba506];
+
+static mut FAKE_TRACK: std::sync::atomic::AtomicUsize = std::sync::atomic::AtomicUsize::new(0);
+//static FAKE_TRACK: *mut std::sync::Mutex<u32> = std::ptr::null_mut();
 
 impl CryptoRandom for FakeCryptoSystem {
     // rust doesn't supply any random functions in std
@@ -17,20 +20,20 @@ impl CryptoRandom for FakeCryptoSystem {
         let mut buffer = buffer.write_lock();
 
         let mut idx = 0;
-        let mut seed = 0;
+        let seed: &mut usize = unsafe { FAKE_TRACK.get_mut() };
 
         for i in 0..buffer.len() {
-            seed = seed
+            *seed = *seed
                 ^ FAKE_SEQ[idx]
                 ^ std::time::SystemTime::now()
                     .duration_since(std::time::UNIX_EPOCH)
                     .unwrap()
-                    .subsec_nanos();
+                    .subsec_nanos() as usize;
             idx += 1;
             if idx >= FAKE_SEQ.len() {
                 idx = 0;
             }
-            buffer[i] = (seed % 256) as u8;
+            buffer[i] = (*seed % 256) as u8;
         }
 
         Ok(())
