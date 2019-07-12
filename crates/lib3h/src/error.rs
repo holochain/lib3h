87cc5eb09e920ccdee70477ee8d416fe09062af4
dymayday <dymayday@gmail.com>
2019-07-12 16:36:52 +0200

    Add custom error

---
+++ crates/lib3h/src/error.rs
@@ -0,0 +1,125 @@
+//! Lib3h custom error definition.
+
+use crate::transport::error::TransportError;
+use lib3h_crypto_api::CryptoError;
+use lib3h_protocol::error::{ErrorKind as Lib3hProtocolErrorKind, Lib3hProtocolError};
+use rmp_serde::decode::Error as RMPSerdeDecodeError;
+use std::{error::Error as StdError, fmt, io, result};
+
+/// A type alias for `Result<T, Lib3hError>`.
+pub type Lib3hResult<T> = result::Result<T, Lib3hError>;
+
+/// An error that can occur when interacting with the algorithm.
+#[derive(Debug)]
+pub struct Lib3hError(Box<ErrorKind>);
+
+impl Lib3hError {
+    /// A constructor for `Lib3hError`.
+    pub fn new(kind: ErrorKind) -> Lib3hError {
+        Lib3hError(Box::new(kind))
+    }
+
+    /// Return the specific type of this error.
+    pub fn kind(&self) -> &ErrorKind {
+        &self.0
+    }
+
+    /// Unwrap this error into its underlying type.
+    pub fn into_kind(self) -> ErrorKind {
+        *self.0
+    }
+}
+
+/// The specific type of an error.
+#[derive(Debug)]
+pub enum ErrorKind {
+    /// An I/O error that occurred while processing a data stream.
+    Io(io::Error),
+    /// Error occuring when using `transport`.
+    TransportError(TransportError),
+    /// Error originating from [lib3h_protocol] crate.
+    Lib3hProtocolError(Lib3hProtocolError),
+    /// Error occuring from [Hcid](hcid) crate.
+    HcId(hcid::HcidError),
+    /// Error originating from [MessagePack](rmp_serde) deserializing crate.
+    RMPSerdeDecodeError(RMPSerdeDecodeError),
+    /// Error from the [lib3h_crypto_api] crate.
+    CryptoAPIError(CryptoError),
+    /// Error occuring when the key is not present in the Map.
+    KeyNotFound(String),
+    /// Yet undefined error.
+    Other(String),
+    /// Hints that destructuring should not be exhaustive.
+    ///
+    /// This enum may grow additional variants, so this makes sure clients
+    /// don't count on exhaustive matching. (Otherwise, adding a new variant
+    /// could break existing code.)
+    #[doc(hidden)]
+    __Nonexhaustive,
+}
+
+impl StdError for Lib3hError {
+    /// The lower-level source of this error, if any.
+    fn source(&self) -> Option<&(dyn StdError + 'static)> {
+        match *self.0 {
+            ErrorKind::Io(ref err) => Some(err),
+            // ErrorKind::SerDeserializeError(ref err) => Some(err),
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl fmt::Display for Lib3hError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self.0 {
+            ErrorKind::Io(ref err) => err.fmt(f),
+            // ErrorKind::SerDeserializeError(ref err) => err.fmt(f),
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl From<io::Error> for Lib3hError {
+    fn from(err: io::Error) -> Self {
+        Lib3hError::new(ErrorKind::Io(err))
+    }
+}
+
+impl From<TransportError> for Lib3hError {
+    fn from(err: TransportError) -> Self {
+        Lib3hError::new(ErrorKind::TransportError(err))
+    }
+}
+
+impl From<hcid::HcidError> for Lib3hError {
+    fn from(err: hcid::HcidError) -> Self {
+        Lib3hError::new(ErrorKind::HcId(err))
+    }
+}
+
+impl From<Lib3hProtocolError> for Lib3hError {
+    fn from(err: Lib3hProtocolError) -> Self {
+        Lib3hError::new(ErrorKind::Lib3hProtocolError(err))
+    }
+}
+
+impl From<RMPSerdeDecodeError> for Lib3hError {
+    fn from(err: RMPSerdeDecodeError) -> Self {
+        Lib3hError::new(ErrorKind::RMPSerdeDecodeError(err))
+    }
+}
+
+impl From<CryptoError> for Lib3hError {
+    fn from(err: CryptoError) -> Self {
+        Lib3hError::new(ErrorKind::CryptoAPIError(err))
+    }
+}
+
+// I'm not so sure about this...
+impl From<Lib3hError> for Lib3hProtocolError {
+    fn from(_err: Lib3hError) -> Self {
+        Lib3hProtocolError::new(Lib3hProtocolErrorKind::Other(String::from(
+            "Lib3hProtocolError occuring in Lib3h.",
+        )))
+    }
+}
---
+++ crates/lib3h_protocol/src/error.rs
@@ -0,0 +1,105 @@
+//! Lib3h_protocol custom error definition.
+
+use std::{error::Error as StdError, fmt, io, result};
+// use serde::Deserializer::Error as DerserializerError;
+use serde::de::value::Error as DeserializeError;
+
+/// A type alias for `Result<T, Lib3hProtocolError>`.
+pub type Lib3hProtocolResult<T> = result::Result<T, Lib3hProtocolError>;
+
+/// An error that can occur when interacting with the algorithm.
+#[derive(Debug)]
+pub struct Lib3hProtocolError(Box<ErrorKind>);
+
+impl Lib3hProtocolError {
+    /// A crate private constructor for `Error`.
+    pub fn new(kind: ErrorKind) -> Lib3hProtocolError {
+        Lib3hProtocolError(Box::new(kind))
+    }
+    /// Return the specific type of this error.
+    pub fn kind(&self) -> &ErrorKind {
+        &self.0
+    }
+
+    /// Unwrap this error into its underlying type.
+    pub fn into_kind(self) -> ErrorKind {
+        *self.0
+    }
+}
+
+/// The specific type of an error.
+#[derive(Debug)]
+pub enum ErrorKind {
+    /// An I/O error that occurred while processing a data stream.
+    Io(io::Error),
+    /// Error occuring when using `transport`.
+    TransportError(String),
+    /// An error occuring whiling trying to deserialize stuff during gossiping for example.
+    // DeserializeError(String),
+    DeserializeError(DeserializeError),
+    /// Yet undefined error.
+    Other(String),
+    /// Hints that destructuring should not be exhaustive.
+    ///
+    /// This enum may grow additional variants, so this makes sure clients
+    /// don't count on exhaustive matching. (Otherwise, adding a new variant
+    /// could break existing code.)
+    #[doc(hidden)]
+    __Nonexhaustive,
+}
+
+impl StdError for Lib3hProtocolError {
+    fn source(&self) -> Option<&(dyn StdError + 'static)> {
+        match *self.0 {
+            ErrorKind::Io(ref err) => Some(err),
+            // ErrorKind::SerDeserializeError(ref err) => Some(err),
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl fmt::Display for Lib3hProtocolError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self.0 {
+            ErrorKind::Io(ref err) => err.fmt(f),
+            // ErrorKind::SerDeserializeError(ref err) => err.fmt(f),
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl From<io::Error> for Lib3hProtocolError {
+    fn from(err: io::Error) -> Self {
+        Lib3hProtocolError::new(ErrorKind::Io(err))
+    }
+}
+
+impl From<DeserializeError> for Lib3hProtocolError {
+    fn from(err: DeserializeError) -> Self {
+        Lib3hProtocolError::new(ErrorKind::DeserializeError(err))
+    }
+}
+
+// impl From<hcid::HcidError> for Lib3hProtocolError {
+//     fn from(err: hcid::HcidError) -> Self {
+//         Lib3hProtocolError::new(ErrorKind::HcId(err))
+//     }
+// }
+
+// impl From<Lib3hProtocolError> for io::Error {
+//     fn from(err: Lib3hProtocolError) -> Self {
+//         io::Error::new(io::ErrorKind::Other, err)
+//     }
+// }
+
+// impl From<transport::error::TransportError> for Lib3hProtocolError {
+//     fn from(err: transport::error::TransportError) -> Self {
+//         Lib3hProtocolError::new(ErrorKind::(err))
+//     }
+// }
+
+// impl From<BincodeError> for Lib3hProtocolError {
+//     fn from(err: BincodeError) -> Self {
+//         new_gen_error(ErrorKind::SerDeserializeError(err))
+//     }
+// }
