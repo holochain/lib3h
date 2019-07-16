705dfd41c40660c7d448e088e984188c465402eb
Chris Belfield <chris@devopshero.io>
2019-07-16 15:14:10 +0100

    removing the update

--- scripts/windows/do-ci-test.bat
+++ scripts/windows/do-ci-test.bat
@@ -2,8 +2,5 @@
 setlocal enabledelayedexpansion
 
 rem KEEP IN SYNC WITH HOLONIX
-set nightly-date=nightly-2019-07-14
-rustup toolchain install --no-self-update !nightly-date!
-rustup default !nightly-date!
 
 cargo test --verbose
