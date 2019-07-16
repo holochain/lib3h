64e5bae5701880527ba56d0bc2d37cd8ce3c5c40
damien <doute.d@gmail.com>
2019-07-15 22:31:57 -0400

    clippy fdp

--- crates/lib3h/src/time.rs
+++ crates/lib3h/src/time.rs
@@ -6,7 +6,7 @@ pub fn since_epoch_ms() -> u64 {
         .duration_since(UNIX_EPOCH)
         .expect("Time went backwards");
     let in_ms =
-        since_the_epoch.as_secs() * 1000 + since_the_epoch.subsec_nanos() as u64 / 1_000_000;
+        since_the_epoch.as_secs() * 1000 + u64::from(since_the_epoch.subsec_nanos()) / 1_000_000;
     in_ms
 }
 
