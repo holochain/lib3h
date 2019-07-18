75d5bcc0fa05a03aeae4f1c112ff609a93f78c9a
damien <doute.d@gmail.com>
2019-07-17 21:11:29 -0400

    always epoch

--- crates/lib3h/src/time.rs
+++ crates/lib3h/src/time.rs
@@ -5,12 +5,12 @@ pub static mut START_TIME: SystemTime = UNIX_EPOCH;
 
 pub fn since_epoch_ms() -> u64 {
     let since_the_epoch;
-    unsafe {
-        let start = SystemTime::now();
-        since_the_epoch = start
-            .duration_since(START_TIME)
-            .expect("Time went backwards");
-    }
+    //unsafe {
+    let start = SystemTime::now();
+    since_the_epoch = start
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+    //}
     let in_ms =
         since_the_epoch.as_secs() * 1000 + u64::from(since_the_epoch.subsec_nanos()) / 1_000_000;
     in_ms
