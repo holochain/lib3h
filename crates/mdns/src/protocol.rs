b07f63efb6ad1aa03d33dcd1df2f94dccc22a815
dymayday <dymayday@gmail.com>
2019-08-02 16:03:17 +0200

    WIP

---
+++ crates/mdns/src/protocol.rs
@@ -0,0 +1,15 @@
+//! Protocol trait definition.
+
+/// Whenever a Multicast DNS responder starts up, wakes up from sleep,
+/// receives an indication of a network interface "Link Change" event, or
+/// has any other reason to believe that its network connectivity may
+/// have changed in some relevant way, it MUST perform the two startup
+/// steps below: [Probing](https://tools.ietf.org/html/rfc6762#section-8.1)
+/// and [Announcing](https://tools.ietf.org/html/rfc6762#section-8.3).
+
+use crate::error::MulticastDnsResult;
+pub trait Discovery {
+    fn probe(&self) -> MulticastDnsResult<()>;
+    fn annonce(&self) -> MulticastDnsResult<()>;
+    fn update(&mut self);
+}
