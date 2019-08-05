1ed21b6ac0c4565fb7320953b09313ff2c7ffebc
David Meister <thedavidmeister@gmail.com>
2019-08-05 20:44:31 +1000

    version 0.0.10

--- scripts/nix/config.nix
+++ scripts/nix/config.nix
@@ -12,14 +12,14 @@
 
    # can be any github ref
    # branch, tag, commit, etc.
-   ref = "0.0.20";
+   ref = "0.0.26";
 
    # the sha of what is downloaded from the above ref
    # note: even if you change the above ref it will not be redownloaded until
    #       the sha here changes (the sha is the cache key for downloads)
    # note: to get a new sha, get nix to try and download a bad sha
    #       it will complain and tell you the right sha
-   sha256 = "0k8d66dl9cwhikwl8a24gxald12lvbl4zf0jxfak9snpnb5mnlnm";
+   sha256 = "1wsb702kdnxg00h08qis963k6c8w3djh24h7r0mgwd2x9ndkasx9";
 
    # the github owner of the holonix repo
    owner = "holochain";
@@ -66,9 +66,9 @@ l3h-release-hook-publish
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.0.9";
+   current = "0.0.10";
    # not used by version hooks in this repo
-   previous = "0.0.8";
+   previous = "0.0.9";
   };
 
   github = {
