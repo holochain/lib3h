953ab9c9cd3cdc96d3224191a213277720d2bb97
Carmelo Piccione <carmelo.piccione@holo.host>
2019-08-08 11:48:13 -0400

    Make dht config fields private

--- crates/lib3h/src/dht/dht_trait.rs
+++ crates/lib3h/src/dht/dht_trait.rs
@@ -13,17 +13,17 @@ pub const DEFAULT_TIMEOUT_THRESHOLD_MS: u64 = 60000;
 
 #[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
 pub struct DhtConfig {
-    pub this_peer_address: PeerAddress,
+    this_peer_address: PeerAddress,
     #[serde(with = "url_serde")]
-    pub this_peer_uri: Url,
-    pub custom: Vec<u8>,
-    pub gossip_interval: u64,
-    pub timeout_threshold: u64,
+    this_peer_uri: Url,
+    custom: Vec<u8>,
+    gossip_interval: u64,
+    timeout_threshold: u64,
 }
 
 impl DhtConfig {
     pub fn new(peer_address: &str, peer_uri: &Url) -> Self {
-        DhtConfig {
+        Self {
             this_peer_address: peer_address.to_owned(),
             this_peer_uri: peer_uri.to_owned(),
             custom: vec![],
@@ -31,7 +31,38 @@ impl DhtConfig {
             timeout_threshold: DEFAULT_TIMEOUT_THRESHOLD_MS,
         }
     }
+
+    pub fn with_real_engine_config(
+        peer_address: &str,
+        peer_uri: &Url,
+        config: &crate::engine::RealEngineConfig,
+    ) -> Self {
+        Self {
+            this_peer_address: peer_address.to_owned(),
+            this_peer_uri: peer_uri.to_owned(),
+            custom: config.clone().dht_custom_config,
+            gossip_interval: config.dht_gossip_interval,
+            timeout_threshold: config.dht_timeout_threshold,
+        }
+    }
+
+    pub fn this_peer_address(&self) -> PeerAddress {
+        self.this_peer_address.clone()
+    }
+
+    pub fn this_peer_uri(&self) -> Url {
+        self.this_peer_uri.clone()
+    }
+
+    pub fn timeout_threshold(&self) -> u64 {
+        self.timeout_threshold
+    }
+
+    pub fn gossip_interval(&self) -> u64 {
+        self.gossip_interval
+    }
 }
+
 pub type DhtFactory<D> = fn(config: &DhtConfig) -> Lib3hResult<D>;
 
 /// Allow storage and retrieval of peer & entry data.
