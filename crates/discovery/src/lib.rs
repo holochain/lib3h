25a094761a6ab1b4ee039ed59ee9f2b8e9eabb22
dymayday <dymayday@gmail.com>
2019-08-22 19:05:37 +0200

    Update Discovery doc

--- crates/discovery/src/lib.rs
+++ crates/discovery/src/lib.rs
@@ -1,11 +1,5 @@
-//! Protocol trait definition.
+//! Discovery trait definition.
 
-/// Whenever a Multicast DNS responder starts up, wakes up from sleep,
-/// receives an indication of a network interface "Link Change" event, or
-/// has any other reason to believe that its network connectivity may
-/// have changed in some relevant way, it MUST perform the two startup
-/// steps below: [Probing](https://tools.ietf.org/html/rfc6762#section-8.1)
-/// and [Announcing](https://tools.ietf.org/html/rfc6762#section-8.3).
 pub mod error;
 pub use error::DiscoveryResult;
 
