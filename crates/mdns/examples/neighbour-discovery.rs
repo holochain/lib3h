e9a3e381f1ac20da3a713441516aa1cfb711600a
dymayday <dymayday@gmail.com>
2019-08-23 16:48:13 +0200

    Update example with comment & release

--- crates/mdns/examples/neighbour-discovery.rs
+++ crates/mdns/examples/neighbour-discovery.rs
@@ -7,18 +7,29 @@ fn discover_neighbourhood() {
         .build()
         .expect("Fail to build mDNS.");
 
-    // Make myself known on the network and find a name for myself
+    // Make myself known on the network and find a name for ourselves
+    println!(">> Advertising...");
     mdns.advertise()
         .expect("Fail to advertise my existence to the world.");
 
+    let mut listen_every_ms = 1_000;
     // Let's listen to the network for a few moments...
     for _ in 0..100 {
         mdns.discover()
             .expect("Fail to discover participants on the network using mDNS.");
-        eprintln!("mDNS neighbourhood : {:#?}", &mdns.records());
+        println!(">> mDNS neighbourhood : {:#?}", &mdns.records());
 
-        mdns::sleep_ms(5_000);
+        // Let's wait a few moments before checking if new participants arrived on the network
+        mdns::sleep_ms(listen_every_ms);
+
+        if listen_every_ms < 30_000 {
+            listen_every_ms += 1_000;
+        }
     }
+
+    // Let's participants know that we are leaving the network.
+    mdns.release()
+        .expect("Fail to release ourselves from the network.");
 }
 
 fn main() {
