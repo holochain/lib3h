0515d7f6570e42a59670bbafa79fb34a732ee907
Eric Harris-Braun <eric@harris-braun.com>
2019-09-03 14:59:47 -0400

    test the response error path

--- crates/lib3h/tests/transport.rs
+++ crates/lib3h/tests/transport.rs
@@ -204,11 +204,21 @@ impl TestTransport {
                 msg.respond(response);
             }
             RequestToChild::SendMessage { address, payload } => {
-                let mut mockernet = MOCKERNET.write().unwrap();
-                // return error if not bound.
-                let _ =
-                    mockernet.send_to(address, self.bound_url.as_ref().unwrap().clone(), payload);
-                //                msg.respond(response);
+                if self.bound_url.is_none() {
+                    msg.respond(Err(TransportError::new(format!("{} not bound", self.name))));
+                } else {
+                    let mut mockernet = MOCKERNET.write().unwrap();
+                    // return error if not bound.
+                    let response = match mockernet.send_to(
+                        address,
+                        self.bound_url.as_ref().unwrap().clone(),
+                        payload,
+                    ) {
+                        Err(err) => Err(TransportError::new(err)),
+                        Ok(()) => Ok(RequestToChildResponse::SendMessage),
+                    };
+                    msg.respond(response);
+                }
             }
         }
         Ok(())
@@ -251,6 +261,7 @@ impl TestTransportOwner {
 
 #[test]
 fn ghost_transport() {
+    // create an object that can be used to hold state data in callbacks to the transports
     let mut owner = TestTransportOwner::new();
 
     let mut t1: TransportActorParentWrapper<(), TestTransport> = GhostParentWrapper::new(
@@ -286,6 +297,31 @@ fn ghost_transport() {
         format!("{:?}", owner.log[0])
     );
 
+    // lets do some things to test out returning of error messages, i.e. sending messages
+    // to someone not bount to the network
+    t1.request(
+        std::time::Duration::from_millis(2000),
+        (),
+        RequestToChild::SendMessage {
+            address: Url::parse("mocknet://t2").expect("can parse url"),
+            payload: b"won't be received!".to_vec(),
+        },
+        // callback should simply log the response
+        Box::new(|dyn_owner, _, response| {
+            let owner = dyn_owner
+                .downcast_mut::<TestTransportOwner>()
+                .expect("a TestTransportOwner");
+            owner.log.push(format!("{:?}", response));
+            Ok(())
+        }),
+    );
+
+    t1.process(&mut owner).expect("should process");
+    assert_eq!(
+        "\"Response(Err(TransportError(\\\"mocknet://t2/ not bound\\\")))\"",
+        format!("{:?}", owner.log[1])
+    );
+
     // bind t2 to the network
     t2.request(
         std::time::Duration::from_millis(2000),
@@ -305,7 +341,7 @@ fn ghost_transport() {
     t2.process(&mut owner).expect("should process");
     assert_eq!(
         "\"Response(Ok(Bind(BindResultData { bound_url: \\\"mocknet://t2/\\\" })))\"",
-        format!("{:?}", owner.log[1])
+        format!("{:?}", owner.log[2])
     );
 
     t1.request(
@@ -324,13 +360,19 @@ fn ghost_transport() {
             Ok(())
         }),
     );
+
+    // we should get back an Ok on having sent the message when t1 gets processed
     t1.process(&mut owner).expect("should process");
+    assert_eq!(
+        "\"Response(Ok(SendMessage))\"",
+        format!("{:?}", owner.log[3])
+    );
+
+    // and when we drain messages from t2 we should see both the incoming connection from t1
+    // and the message itself
     t2.process(&mut owner).expect("should process");
     let mut messages = t2.drain_messages();
     assert_eq!(messages.len(), 2);
-
-    // because this is the first incoming message, the low level
-    // transport should also send an IncomingConnection event
     assert_eq!(
         "IncomingConnection { address: \"mocknet://t1/\" }",
         format!("{:?}", messages[0].take_message().expect("exists"))
