7cdff49dc897e120f8f430225b894fd56b4fdafe
neonphog <neonphog@gmail.com>
2019-09-06 13:41:04 -0600

    transport ghost as legacy

---
+++ crates/lib3h/src/transport/transport_ghost_as_legacy.rs
@@ -0,0 +1,221 @@
+use crate::transport::{error::*, protocol::*, ConnectionId, ConnectionIdRef, Transport};
+use detach::prelude::*;
+use lib3h_ghost_actor::prelude::*;
+use lib3h_protocol::DidWork;
+use url::Url;
+
+#[derive(Debug)]
+enum ToInnerContext {
+    None,
+}
+
+pub struct TransportGhostAsLegacy {
+    // ref to our inner transport
+    inner_transport: Detach<TransportActorParentWrapperDyn<TransportGhostAsLegacy, ToInnerContext>>,
+    outbox: Vec<TransportEvent>,
+    sync_ready: bool,
+    bound_url: Url,
+}
+
+impl TransportGhostAsLegacy {
+    /// create a new TransportGhostAsLegacy Instance
+    pub fn new(inner_transport: DynTransportActor) -> Self {
+        let inner_transport = Detach::new(GhostParentWrapperDyn::new(
+            inner_transport,
+            "mplex_to_inner_",
+        ));
+        Self {
+            inner_transport,
+            outbox: Vec::new(),
+            sync_ready: false,
+            bound_url: Url::parse("none:").expect("can parse url"),
+        }
+    }
+
+    /// private dispatcher for messages from our inner transport
+    fn handle_msg_from_inner(
+        &mut self,
+        mut msg: GhostMessage<
+            RequestToParent,
+            RequestToChild,
+            RequestToParentResponse,
+            TransportError,
+        >,
+    ) -> TransportResult<()> {
+        match msg.take_message().expect("exists") {
+            RequestToParent::IncomingConnection { address } => {
+                self.handle_incoming_connection(address)
+            }
+            RequestToParent::ReceivedData { address, payload } => {
+                self.handle_received_data(address, payload)
+            }
+            RequestToParent::TransportError { error } => self.handle_transport_error(error),
+        }
+    }
+
+    /// private handler for inner transport IncomingConnection events
+    fn handle_incoming_connection(&mut self, address: Url) -> TransportResult<()> {
+        // forward
+        self.outbox
+            .push(TransportEvent::IncomingConnectionEstablished(
+                address.to_string(),
+            ));
+        Ok(())
+    }
+
+    /// private handler for inner transport ReceivedData events
+    fn handle_received_data(&mut self, address: Url, payload: Vec<u8>) -> TransportResult<()> {
+        // forward
+        self.outbox
+            .push(TransportEvent::ReceivedData(address.to_string(), payload));
+        Ok(())
+    }
+
+    /// private handler for inner transport TransportError events
+    fn handle_transport_error(&mut self, error: TransportError) -> TransportResult<()> {
+        panic!("cannot handle transport errors here: {:?}", error);
+    }
+
+    fn priv_process(&mut self) -> TransportResult<()> {
+        detach_run!(&mut self.inner_transport, |it| it.process(self))?;
+        for msg in self.inner_transport.as_mut().drain_messages() {
+            self.handle_msg_from_inner(msg)?;
+        }
+        Ok(())
+    }
+
+    /// private handler for Bind requests from our parent
+    fn priv_bind(&mut self, spec: Url) -> TransportResult<Url> {
+        self.sync_ready = false;
+
+        // forward the bind to our inner_transport
+        self.inner_transport.as_mut().request(
+            ToInnerContext::None,
+            RequestToChild::Bind { spec },
+            Box::new(|me, _, response| {
+                me.sync_ready = true;
+                let response = {
+                    match response {
+                        GhostCallbackData::Timeout => {
+                            return Err("timeout".into());
+                        }
+                        GhostCallbackData::Response(response) => response,
+                    }
+                };
+                match response {
+                    Ok(RequestToChildResponse::Bind(BindResultData { bound_url })) => {
+                        me.bound_url = bound_url;
+                    }
+                    _ => return Err("invalid".into()),
+                }
+                Ok(())
+            }),
+        )?;
+
+        loop {
+            self.priv_process()?;
+            if self.sync_ready {
+                break;
+            }
+        }
+
+        Ok(self.bound_url.clone())
+    }
+
+    /// private handler for SendMessage requests from our parent
+    fn priv_send_message(&mut self, address: Url, payload: Vec<u8>) -> TransportResult<()> {
+        self.sync_ready = false;
+
+        // forward the request to our inner_transport
+        self.inner_transport.as_mut().request(
+            ToInnerContext::None,
+            RequestToChild::SendMessage { address, payload },
+            Box::new(|me, _, _| {
+                me.sync_ready = true;
+                Ok(())
+            }),
+        )?;
+
+        loop {
+            self.priv_process()?;
+            if self.sync_ready {
+                break;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+impl Transport for TransportGhostAsLegacy {
+    fn connect(&mut self, uri: &Url) -> TransportResult<ConnectionId> {
+        // psyche!
+        Ok(uri.to_string())
+    }
+
+    fn close(&mut self, _id: &ConnectionIdRef) -> TransportResult<()> {
+        // no-op
+        Ok(())
+    }
+
+    fn close_all(&mut self) -> TransportResult<()> {
+        // no-op
+        Ok(())
+    }
+
+    fn send(&mut self, id_list: &[&ConnectionIdRef], payload: &[u8]) -> TransportResult<()> {
+        for id in id_list {
+            self.priv_send_message(Url::parse(id).expect("can parse url"), payload.to_vec())?;
+        }
+        Ok(())
+    }
+
+    fn send_all(&mut self, _payload: &[u8]) -> TransportResult<()> {
+        unimplemented!();
+    }
+
+    fn bind(&mut self, url: &url::Url) -> TransportResult<Url> {
+        self.priv_bind(url.clone())
+    }
+
+    fn post(&mut self, command: TransportCommand) -> TransportResult<()> {
+        match command {
+            TransportCommand::Connect(url, _rid) => {
+                self.connect(&url)?;
+            }
+            TransportCommand::Send(id_list, payload) => {
+                self.send(
+                    &id_list.iter().map(|x| x.as_str()).collect::<Vec<_>>(),
+                    &payload,
+                )?;
+            }
+            TransportCommand::SendAll(payload) => {
+                self.send_all(&payload)?;
+            }
+            TransportCommand::Close(id) => {
+                self.close(&id)?;
+            }
+            TransportCommand::CloseAll => {
+                self.close_all()?;
+            }
+            TransportCommand::Bind(url) => {
+                self.bind(&url)?;
+            }
+        };
+        Ok(())
+    }
+
+    fn process(&mut self) -> TransportResult<(DidWork, Vec<TransportEvent>)> {
+        self.priv_process()?;
+        Ok((true, self.outbox.drain(..).collect()))
+    }
+
+    fn connection_id_list(&self) -> TransportResult<Vec<ConnectionId>> {
+        unimplemented!();
+    }
+
+    fn get_uri(&self, id: &ConnectionIdRef) -> Option<Url> {
+        // psyche!
+        Some(Url::parse(id).expect("can parse url"))
+    }
+}
---
+++ crates/lib3h/src/transport/transport_legacy_as_ghost.rs
@@ -0,0 +1,145 @@
+use crate::transport::{error::*, protocol::*, ConnectionId};
+use detach::prelude::*;
+use lib3h_ghost_actor::prelude::*;
+use std::collections::HashMap;
+use url::Url;
+
+use super::TransportWrapper;
+
+pub struct TransportLegacyAsGhost<'gt> {
+    // our parent channel endpoint
+    endpoint_parent: Option<TransportActorParentEndpoint>,
+    // our self channel endpoint
+    endpoint_self: Detach<TransportActorSelfEndpoint<TransportLegacyAsGhost<'gt>, ()>>,
+    // ref to our inner legacy transport
+    inner_transport: TransportWrapper<'gt>,
+    url_to_con_id: HashMap<Url, ConnectionId>,
+    con_id_to_url: HashMap<ConnectionId, Url>,
+}
+
+impl<'gt> TransportLegacyAsGhost<'gt> {
+    /// create a new TransportLegacyAsGhost Instance
+    pub fn new(inner_transport: TransportWrapper<'gt>) -> Self {
+        let (endpoint_parent, endpoint_self) = create_ghost_channel();
+        let endpoint_parent = Some(endpoint_parent);
+        let endpoint_self = Detach::new(
+            endpoint_self
+                .as_context_endpoint_builder()
+                .request_id_prefix("mplex_to_parent_")
+                .build(),
+        );
+        Self {
+            endpoint_parent,
+            endpoint_self,
+            inner_transport,
+            url_to_con_id: HashMap::new(),
+            con_id_to_url: HashMap::new(),
+        }
+    }
+
+    /// private dispatcher for messages coming from our parent
+    fn handle_msg_from_parent(
+        &mut self,
+        mut msg: GhostMessage<
+            RequestToChild,
+            RequestToParent,
+            RequestToChildResponse,
+            TransportError,
+        >,
+    ) -> TransportResult<()> {
+        match msg.take_message().expect("exists") {
+            RequestToChild::Bind { spec } => self.handle_bind(msg, spec),
+            RequestToChild::SendMessage { address, payload } => {
+                self.handle_send_message(msg, address, payload)
+            }
+        }
+    }
+
+    /// private handler for Bind requests from our parent
+    fn handle_bind(
+        &mut self,
+        msg: GhostMessage<RequestToChild, RequestToParent, RequestToChildResponse, TransportError>,
+        spec: Url,
+    ) -> TransportResult<()> {
+        // forward the bind to our inner_transport
+        let bound_url = self.inner_transport.as_mut().bind(&spec)?;
+        msg.respond(Ok(RequestToChildResponse::Bind(BindResultData {
+            bound_url,
+        })))?;
+        Ok(())
+    }
+
+    /// private handler for SendMessage requests from our parent
+    fn handle_send_message(
+        &mut self,
+        msg: GhostMessage<RequestToChild, RequestToParent, RequestToChildResponse, TransportError>,
+        address: Url,
+        payload: Vec<u8>,
+    ) -> TransportResult<()> {
+        if !self.url_to_con_id.contains_key(&address) {
+            // connect if needed
+            let con_id = self.inner_transport.as_mut().connect(&address)?;
+            self.url_to_con_id.insert(address.clone(), con_id.clone());
+            self.con_id_to_url.insert(con_id.clone(), address.clone());
+        }
+        let con_id = self.url_to_con_id.get(&address).unwrap().clone();
+        // forward the request to our inner_transport
+        self.inner_transport.as_mut().send(&[&con_id], &payload)?;
+        msg.respond(Ok(RequestToChildResponse::SendMessage))?;
+        Ok(())
+    }
+}
+
+impl<'gt>
+    GhostActor<
+        RequestToParent,
+        RequestToParentResponse,
+        RequestToChild,
+        RequestToChildResponse,
+        TransportError,
+    > for TransportLegacyAsGhost<'gt>
+{
+    fn take_parent_endpoint(&mut self) -> Option<TransportActorParentEndpoint> {
+        std::mem::replace(&mut self.endpoint_parent, None)
+    }
+
+    fn process_concrete(&mut self) -> GhostResult<WorkWasDone> {
+        detach_run!(&mut self.endpoint_self, |es| es.process(self))?;
+        for msg in self.endpoint_self.as_mut().drain_messages() {
+            self.handle_msg_from_parent(msg)?;
+        }
+        let (_did_work, events) = self.inner_transport.as_mut().process()?;
+        for ev in events {
+            match ev {
+                TransportEvent::ErrorOccured(id, _e) => {
+                    // just delete our interal tracking things
+                    match self.con_id_to_url.remove(&id) {
+                        None => (),
+                        Some(address) => {
+                            self.url_to_con_id.remove(&address);
+                        }
+                    }
+                }
+                TransportEvent::ConnectResult(_id, _r) => {
+                    // ??
+                }
+                TransportEvent::IncomingConnectionEstablished(id) => {
+                    let address = self.inner_transport.as_ref().get_uri(&id).unwrap();
+                    self.con_id_to_url.insert(id.clone(), address.clone());
+                    self.url_to_con_id.insert(address.clone(), id.clone());
+                    self.endpoint_self
+                        .publish(RequestToParent::IncomingConnection { address })?;
+                }
+                TransportEvent::ConnectionClosed(_id) => {}
+                TransportEvent::ReceivedData(id, payload) => {
+                    let address = self.inner_transport.as_ref().get_uri(&id).unwrap();
+                    self.con_id_to_url.insert(id.clone(), address.clone());
+                    self.url_to_con_id.insert(address.clone(), id.clone());
+                    self.endpoint_self
+                        .publish(RequestToParent::ReceivedData { address, payload })?;
+                }
+            }
+        }
+        Ok(false.into())
+    }
+}
