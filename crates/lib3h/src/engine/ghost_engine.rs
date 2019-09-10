6873e429291dfa7ed7b7319ca3a3082633e45186
Eric Harris-Braun <eric@harris-braun.com>
2019-09-10 13:46:14 -0400

    clippy fixes

--- crates/lib3h/src/engine/ghost_engine.rs
+++ crates/lib3h/src/engine/ghost_engine.rs
@@ -591,7 +591,7 @@ impl<'engine> GhostEngine<'engine> {
         let space_gateway = self
             .space_gateway_map
             .get_mut(&chain_id)
-            .ok_or(Lib3hError::new_other("Not part of that space"))?;
+            .ok_or_else(|| Lib3hError::new_other("Not part of that space"))?;
 
         // Check if messaging self
         let peer_address = { space_gateway.as_mut().get_this_peer_sync().peer_address };
@@ -671,7 +671,7 @@ impl<'engine> GhostEngine<'engine> {
         let _space_gateway = self
             .space_gateway_map
             .get_mut(&chain_id)
-            .ok_or(Lib3hError::new_other("Not part of that space"))?;
+            .ok_or_else(|| Lib3hError::new_other("Not part of that space"))?;
         /*
                 let context = "".to_string();
                     //DhtContext::RequestEntry { }
@@ -724,10 +724,9 @@ impl<'engine> GhostEngine<'engine> {
     ) -> Lib3hResult<&mut GatewayWrapper<'engine>> {
         self.space_gateway_map
             .get_mut(&(space_address.to_owned(), agent_id.to_owned()))
-            .ok_or(Lib3hError::new_other(&format!(
-                "Not in space: {:?},{:?}",
-                space_address, agent_id
-            )))
+            .ok_or_else(|| {
+                Lib3hError::new_other(&format!("Not in space: {:?},{:?}", space_address, agent_id))
+            })
     }
 }
 
