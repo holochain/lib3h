ec22d39d3fccec883f3a85de533d75fd5aa46953
neonphog <neonphog@gmail.com>
2019-09-26 13:48:33 -0600

    bench on appveyor

--- scripts/windows/do-ci-test.bat
+++ scripts/windows/do-ci-test.bat
@@ -4,4 +4,5 @@ setlocal enabledelayedexpansion
 rem KEEP IN SYNC WITH HOLONIX
 set RUST_LOG=lib3h=debug
 set RUST_BACKTRACE=1
+cargo bench -p lib3h --target-dir c:\build\lib3h\target
 cargo test --target-dir c:\build\lib3h\target --verbose
