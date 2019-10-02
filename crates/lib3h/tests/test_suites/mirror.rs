133272850d6465b4359c35fb17ef2ad8a5e8abc2
Eric Harris-Braun <eric@harris-braun.com>
2019-10-02 11:40:08 -0400

    add processing time and check for stored entry

--- crates/lib3h/tests/test_suites/mirror.rs
+++ crates/lib3h/tests/test_suites/mirror.rs
@@ -43,19 +43,36 @@ fn test_setup_only(_nodes: &mut Vec<NodeMock>) {
 }
 
 fn test_mirror(nodes: &mut Vec<NodeMock>) {
-    let mut node1 = nodes.pop().unwrap();
-    let mut node2 = nodes.pop().unwrap();
-    {
-        // node1 publishes data on the network
-        let _entry = node1
+    let entry = {
+        let mut node1 = nodes.pop().unwrap();
+        let mut node2 = nodes.pop().unwrap();
+
+            // node1 publishes data on the network
+        let entry = node1
             .author_entry(&ENTRY_ADDRESS_1, vec![ASPECT_CONTENT_1.clone()], true)
             .unwrap();
+
+        let expected = "HandleStoreEntryAspect\\(StoreEntryAspectData \\{ request_id: \"[\\w\\d_~]+\", space_address: HashString\\(\"\\w+\"\\), provider_agent_id: HashString\\(\"mirror_node9\"\\), entry_address: HashString\\(\"entry_addr_1\"\\), entry_aspect: EntryAspectData \\{ aspect_address: HashString\\(\"[\\w\\d]+\"\\), type_hint: \"NodeMock\", aspect: \"hello-1\", publish_ts: \\d+ \\} \\}\\)";
+
+        let _results = assert2_msg_matches!(node1, node2, expected);
+
+        assert_eq!(entry, node1.get_entry(&ENTRY_ADDRESS_1).unwrap());
+        nodes.push(node2);
+        nodes.push(node1);
+        entry
+    };
+
+    for _i in 0..20 {
+        proc(nodes);
     }
-    let expected = "HandleStoreEntryAspect\\(StoreEntryAspectData \\{ request_id: \"[\\w\\d_~]+\", space_address: HashString\\(\"\\w+\"\\), provider_agent_id: HashString\\(\"mirror_node9\"\\), entry_address: HashString\\(\"entry_addr_1\"\\), entry_aspect: EntryAspectData \\{ aspect_address: HashString\\(\"[\\w\\d]+\"\\), type_hint: \"NodeMock\", aspect: \"hello-1\", publish_ts: \\d+ \\} \\}\\)";
 
-    let _results = assert2_msg_matches!(node1, node2, expected);
-    nodes.push(node2);
-    nodes.push(node1);
+    for node in nodes {
+        assert_eq!(entry, node.get_entry(&ENTRY_ADDRESS_1).unwrap());
+    }
+}
 
-    // TODO: the rest of the code that proves out mirroring
+fn proc(nodes: &mut Vec<NodeMock>) {
+    for node in nodes {
+        let _result = node.process();
+    }
 }
