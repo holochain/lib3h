8b75c803f6cf9af4fdb690f0a220cdb008c1d61c
dymayday <dymayday@gmail.com>
2019-10-07 15:58:23 +0200

    gateway actor Bubble up timeout errors

--- crates/lib3h/src/gateway/gateway_actor.rs
+++ crates/lib3h/src/gateway/gateway_actor.rs
@@ -44,7 +44,7 @@ impl
             Box::new(|mut me, response| {
                 let response = {
                     match response {
-                        GhostCallbackData::Timeout(bt) => panic!("timeout: {:?}", bt),
+                        GhostCallbackData::Timeout(bt) => return Err(Lib3hError::new_timeout(&bt).into()),
                         GhostCallbackData::Response(response) => match response {
                             Err(e) => panic!("{:?}", e),
                             Ok(response) => response,
