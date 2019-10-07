e7b79986dcc7bdb55f30a3ae07f524f50aefcfd7
dymayday <dymayday@gmail.com>
2019-10-07 18:28:18 +0200

    Version bump before release

--- scripts/nix/config.nix
+++ scripts/nix/config.nix
@@ -66,9 +66,9 @@ l3h-release-hook-publish
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.0.10";
+   current = "0.0.11";
    # not used by version hooks in this repo
-   previous = "0.0.9";
+   previous = "0.0.10";
   };
 
   github = {
