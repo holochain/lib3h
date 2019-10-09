58ab004d1d9a519b976f2bb881fd2be045a39bae
Carmelo Piccione <carmelo.piccione@holo.host>
2019-10-08 23:27:19 -0400

    Set trace level logger in l3h-test script

--- scripts/nix/test/default.nix
+++ scripts/nix/test/default.nix
@@ -5,7 +5,7 @@ let
   script = pkgs.writeShellScriptBin name
   ''
   echo BACKTRACE_STRATEGY=$BACKTRACE_STRATEGY
-  RUST_BACKTRACE=1 \
+  RUST_LOG=trace RUST_BACKTRACE=1 \
   hn-rust-fmt-check \
   && hn-rust-clippy \
   && cargo test
