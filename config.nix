aa34412724e7ca4e5f08d562653d1da892178ee5
Eric Harris-Braun <eric@harris-braun.com>
2019-10-15 06:30:52 -0400

    release preflight for special 0.0.16 release

--- config.nix
+++ config.nix
@@ -60,15 +60,15 @@ l3h-release-hook-publish
   # the commit hash that the release process should target
   # this will always be behind what ends up being deployed
   # the release process needs to add some commits for changelog etc.
-  commit = "5f5982845b093eed1bde73c20cc9b911d1784d74";
+  commit = "5f9995dd80b18b58e754fac71345307e56354a66";
 
   # the semver for prev and current releases
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.0.14";
+   current = "0.0.16";
    # not used by version hooks in this repo
-   previous = "0.0.13";
+   previous = "0.0.14";
   };
 
   github = {
