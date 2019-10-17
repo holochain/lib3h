498227c5f3b53854f6264b3cf50cda900b8a69d6
David Meister <thedavidmeister@gmail.com>
2019-10-17 21:13:03 +1100

    release prep

--- config.nix
+++ config.nix
@@ -60,15 +60,15 @@ l3h-release-hook-publish
   # the commit hash that the release process should target
   # this will always be behind what ends up being deployed
   # the release process needs to add some commits for changelog etc.
-  commit = "ce2cd1177e01b94ce894a6644a6df9ed8b90aa8c";
+  commit = "08d9f7fb2c8d9cb9bf5bc639c52823824b118d10";
 
   # the semver for prev and current releases
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.0.17";
+   current = "0.0.18";
    # not used by version hooks in this repo
-   previous = "0.0.16";
+   previous = "0.0.17";
   };
 
   github = {
