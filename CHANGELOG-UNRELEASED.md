278cbe12456f23159afc5572970181fff2cf4ff7
neonphog <neonphog@gmail.com>
2019-10-28 11:09:29 -0600

    release prep

--- CHANGELOG-UNRELEASED.md
+++ CHANGELOG-UNRELEASED.md
@@ -6,6 +6,8 @@ This project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.htm
 
 ### Added
 
+- ghost_mutex for lock timeouts
+
 ### Changed
 
 ### Deprecated
--- config.nix
+++ config.nix
@@ -60,15 +60,15 @@ l3h-release-hook-publish
   # the commit hash that the release process should target
   # this will always be behind what ends up being deployed
   # the release process needs to add some commits for changelog etc.
-  commit = "f66fb2d8345a5a03952de04ea615cec6c1f244d9";
+  commit = "c6b7bd75c4cadf52cbb5af2d78ecb311afa1b80a";
 
   # the semver for prev and current releases
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.0.19";
+   current = "0.0.20";
    # not used by version hooks in this repo
-   previous = "0.0.18";
+   previous = "0.0.19";
   };
 
   github = {
