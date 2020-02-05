b39ba24e690993f1baa342e7190215bd4813e2c5
Ashanti Mutinta <Ashantimutinta@gmail.com>
2020-02-05 10:12:11 -0500

    update version numbers of crate

--- config.nix
+++ config.nix
@@ -66,9 +66,9 @@ echo 'Check circle CI for crates-io publishing!'
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.0.32";
+   current = "0.0.33";
    # not used by version hooks in this repo
-   previous = "0.0.31";
+   previous = "0.0.32";
   };
 
   github = {
