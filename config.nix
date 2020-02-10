fb7b571badfbe415a165b18de42d3f1a6295a0a4
David Meister <thedavidmeister@gmail.com>
2020-02-11 01:05:41 +0400

    Release v0.0.36

--- config.nix
+++ config.nix
@@ -60,13 +60,13 @@ echo 'Check circle CI for crates-io publishing!'
   # the commit hash that the release process should target
   # this will always be behind what ends up being deployed
   # the release process needs to add some commits for changelog etc.
-  commit = "4f958c3c497c3805f746b15b1065a242f5c64a5d";
+  commit = "c5c42833b20b82b8658920a2e6836fc2cea0018a";
 
   # the semver for prev and current releases
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.0.34";
+   current = "0.0.36";
    # not used by version hooks in this repo
    previous = "_._._";
   };
